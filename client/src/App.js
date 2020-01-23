3c25f0336d162e6e8005ed14a9bf3cb75c65f733
Zack Moscow <zmoscow@gmail.com>
2020-01-23 13:04:31 -0600

    initial file structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  );
+}
+
+export default App;
