3b1744ee261d164231c483293ce65daa2a2f501c
Tony Sarni <sarni.tony@gmail.com>
2020-01-24 15:26:00 -0600

    initial draft of models

---
+++ models/game.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+    participant: {
+        type: String,
+        score: Number,
+        avatar: String,
+        position: Number,
+        eliminated: Boolean
+    },
+    round: {
+        question: String,
+        response: Array,
+            
+
+    }
+ 
+});
+
+const Game = mongoose.model("Game", productSchema);
+
+module.exports = Game;
