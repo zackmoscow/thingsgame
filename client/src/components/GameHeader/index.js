0bd095207e5ca2b124b785d8c02bb6501948a80f
jhumbrac <john.humbracht@generalassemb.ly>
2020-01-31 12:05:01 -0600

    finishing frontend for offline

--- client/src/components/GameHeader/index.js
+++ client/src/components/GameHeader/index.js
@@ -4,10 +4,14 @@ import "./style.css";
 export default function GameHeader(props) {
     return (
         <div className="headerArea">
-            <h1>Round {props.round}</h1>
-            <h2>promptmaster {(props.promptmaster) ? props.promptmaster.name : null}</h2>
-            <h2>Question: {(props.roundQuestion) ? props.roundQuestion : null}</h2>
-            <h2>Matcher: {(props.matcher) ? props.matcher.name : null}</h2>
+            <div className="leftHeader">
+                <h2 className="round"><span>Round</span> {props.round}</h2>
+                <h2 className="promptmaster"><span>promptmaster</span> {(props.promptmaster) ? props.promptmaster.name : "waiting"}</h2>
+            </div>
+            <div className="rightHeader">
+                <h1 className="prompt"><span>Prompt</span> {(props.roundQuestion) ? props.roundQuestion : "waiting"}</h1>
+                <h2 className="matching"><span>Matching</span> {(props.matcher) ? props.matcher.name : "waiting"}</h2>
+            </div>
         </div>
     )
 }
