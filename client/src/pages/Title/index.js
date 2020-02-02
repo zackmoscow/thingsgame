b0d6e05d670d1c627131370fe754b98bc241f189
jhumbrac <john.humbracht@generalassemb.ly>
2020-02-02 09:08:00 -0600

    uploading ui changes and working avatar form

--- client/src/pages/Title/index.js
+++ client/src/pages/Title/index.js
@@ -189,7 +189,7 @@ export default function Title() {
     }
 
     return (
-        <section className="titleScreen">
+        <section className="titleScreen wrapper">
             <Logo />
             {isLoggedIn()}
             {showForm()}
